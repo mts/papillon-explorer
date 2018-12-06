364911cdd2610b73e76bc890d261700fda2adc10
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-06 22:57:40 +0100

    Add client pages

---
+++ packages/client/src/components/pages/BlankslatePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BlankslatePage = () => {
+  return (
+    <div>
+      <h2>Blankslate Page</h2>
+      <div>
+        Under construction 🙂
+        <Link to="/">Back to Home Page</Link>
+      </div>
+    </div>
+  )
+}
+
+export default BlankslatePage;
