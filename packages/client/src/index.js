05beb0fa482972c1c5a895b215c22a5edfa0a080
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-07 17:15:09 +0100

    Restore start page

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -10,7 +10,6 @@ import ButtonsPage from './components/pages/ButtonsPage';
 import LabelsPage from './components/pages/LabelsPage';
 import LayoutPage from './components/pages/LayoutPage';
 import NavigationPage from './components/pages/NavigationPage';
-import './index.scss'
 
 function addDivWithIdToBody() {
   const appElement = document.createElement("div");
