bb296dad63102f38d607111d3c5bc7f84ec6b9c5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-09 16:53:35 +0100

    Add all variants

--- packages/client/src/components/pages/LabelsPage/index.js
+++ packages/client/src/components/pages/LabelsPage/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import RegularLabels from './RegularLabels'
+import ThemeLabels from './ThemeLabels'
+import StateLabels from './StateLabels'
+import CounterLabels from './CounterLabels'
 
-const LabelsPage = () => {
+const LayoutPage = () => {
   return (
     <div>
-      <h2>Labels Page</h2>
-      <div>
-        Under construction 🙂🙂🙂
+      <h1 align="center" class="p-4">🎨 Papillon Labels Component Explorer</h1>
+      <div className="d-flex flex-justify-around">
+        <div className="col-3">
+          <RegularLabels />
+        </div>
+        <div className="col-3">
+          <ThemeLabels />
+        </div>
+        <div className="col-3">
+          <StateLabels />
+        </div>
+        <div className="col-3">
+          <CounterLabels />
+        </div>
+      </div>
+      <div className="p-4">
         <Link to="/">Back to Home Page</Link>
       </div>
     </div>
   )
 }
 
-export default LabelsPage;
+export default LayoutPage;
