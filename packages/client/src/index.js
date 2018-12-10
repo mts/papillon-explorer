ae5158aa73b3fb0c8230ea34c51b65f72b826876
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-10 19:44:40 +0100

    Add routes

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import StartPage from './components/pages/StartPage';
 import AlertsPage from './components/pages/AlertsPage';
 import AvatarsPage from './components/pages/AvatarsPage';
@@ -10,6 +10,18 @@ import ButtonsPage from './components/pages/ButtonsPage';
 import LabelsPage from './components/pages/LabelsPage';
 import LayoutPage from './components/pages/LayoutPage';
 import NavigationPage from './components/pages/NavigationPage';
+import './index.scss'
+import {
+  startPageRoute,
+  alertsPageRoute,
+  avatarsPageRoute,
+  blankslatePageRoute,
+  breadcrumbPageRoute,
+  buttonsPageRoute,
+  labelsPageRoute,
+  layoutPageRoute,
+  navigationPageRoute,
+} from './routes'
 
 function addDivWithIdToBody() {
   const appElement = document.createElement("div");
@@ -22,17 +34,17 @@ addDivWithIdToBody()
 const App = () => {
   return (
     <Router>
-      <div>
-        <Route path="/" component={StartPage} />
-        <Route path="/alerts" component={AlertsPage} />
-        <Route path="/avatars" component={AvatarsPage} />
-        <Route path="/blankslate" component={BlankslatePage} />
-        <Route path="/breadcrumb" component={BreadcrumbPage} />
-        <Route path="/buttons" component={ButtonsPage} />
-        <Route path="/labels" component={LabelsPage} />
-        <Route path="/layout" component={LayoutPage} />
-        <Route path="/navigation" component={NavigationPage} />
-      </div>
+      <Switch>
+        <Route exact path={startPageRoute} component={StartPage} />
+        <Route exact path={alertsPageRoute} component={AlertsPage} />
+        <Route exact path={avatarsPageRoute} component={AvatarsPage} />
+        <Route exact path={blankslatePageRoute} component={BlankslatePage} />
+        <Route exact path={breadcrumbPageRoute} component={BreadcrumbPage} />
+        <Route exact path={buttonsPageRoute} component={ButtonsPage} />
+        <Route exact path={labelsPageRoute} component={LabelsPage} />
+        <Route exact path={layoutPageRoute} component={LayoutPage} />
+        <Route exact path={navigationPageRoute} component={NavigationPage} />
+      </Switch>
     </Router>
   );
 }
