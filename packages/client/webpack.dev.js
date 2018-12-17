23671879a02eb00f69b37ab30895a75f658525b9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-17 15:30:42 +0100

    Refine webpack dev config

--- packages/client/webpack.dev.js
+++ packages/client/webpack.dev.js
@@ -8,11 +8,6 @@ const path = require('path');
 const sane = require('sane');
 const webpack = require('webpack');
 
-// webpack plugins
-// const Dashboard = require('webpack-dashboard');
-// const DashboardPlugin = require('webpack-dashboard/plugin');
-// const dashboard = new Dashboard();
-
 // config files
 const common = require('./webpack.common.js');
 const pkg = require('./package.json');
@@ -38,17 +33,6 @@ const configureDevServer = (buildType) => {
         headers: {
             'Access-Control-Allow-Origin': '*'
         },
-        // // Use sane to monitor all of the templates files and sub-directories
-        // before: (app, server) => {
-        //     const watcher = sane(path.join(__dirname, settings.paths.templates), {
-        //         glob: ['**/*'],
-        //         poll: !!parseInt(settings.devServerConfig.poll()),
-        //     });
-        //     watcher.on('change', function(filePath, root, stat) {
-        //         console.log('  File modified:', filePath);
-        //         server.sockWrite(server.sockets, "content-changed");
-        //     });
-        // },
     };
 };
 
@@ -164,7 +148,6 @@ module.exports = [
             },
             plugins: [
                 new webpack.HotModuleReplacementPlugin(),
-                // new DashboardPlugin(dashboard.setData),
             ],
         }
     ),
