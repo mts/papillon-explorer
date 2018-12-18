ffac1344b8db502ec491cd1e87bc054a272cfcb7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-18 15:11:02 +0100

    Add apollo client

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 import './index.scss'
 import {
   startPageRoute,
@@ -15,32 +15,33 @@ import {
   navigationPageRoute,
 } from './routes'
 import '@babel/polyfill'
+import client from './graphql/client'
 
-const StartPage = React.lazy(() => import('./components/pages/StartPage'))
+const StartPage = React.lazy(() => import('./pages/StartPage'))
 const asyncStartPage = () => <React.Suspense fallback={null}><StartPage /></React.Suspense>
 
-const AlertsPage = React.lazy(() => import('./components/pages/AlertsPage'))
+const AlertsPage = React.lazy(() => import('./pages/AlertsPage'))
 const asyncAlertsPage = () => <React.Suspense fallback={null}><AlertsPage /></React.Suspense>
 
-const AvatarsPage = React.lazy(() => import('./components/pages/AvatarsPage'))
+const AvatarsPage = React.lazy(() => import('./pages/AvatarsPage'))
 const asyncAvatarsPage = () => <React.Suspense fallback={null}><AvatarsPage /></React.Suspense>
 
-const BlankslatePage = React.lazy(() => import('./components/pages/BlankslatePage'))
+const BlankslatePage = React.lazy(() => import('./pages/BlankslatePage'))
 const asyncBlankslatePage = () => <React.Suspense fallback={null}><BlankslatePage /></React.Suspense>
 
-const BreadcrumbPage = React.lazy(() => import('./components/pages/BreadcrumbPage'))
+const BreadcrumbPage = React.lazy(() => import('./pages/BreadcrumbPage'))
 const asyncBreadcrumbPage = () => <React.Suspense fallback={null}><BreadcrumbPage /></React.Suspense>
 
-const ButtonsPage = React.lazy(() => import('./components/pages/ButtonsPage'))
+const ButtonsPage = React.lazy(() => import('./pages/ButtonsPage'))
 const asyncButtonsPage = () => <React.Suspense fallback={null}><ButtonsPage /></React.Suspense>
 
-const LabelsPage = React.lazy(() => import('./components/pages/LabelsPage'))
+const LabelsPage = React.lazy(() => import('./pages/LabelsPage'))
 const asyncLabelsPage = () => <React.Suspense fallback={null}><LabelsPage /></React.Suspense>
 
-const LayoutPage = React.lazy(() => import('./components/pages/LayoutPage'))
+const LayoutPage = React.lazy(() => import('./pages/LayoutPage'))
 const asyncLayoutPage = () => <React.Suspense fallback={null}><LayoutPage /></React.Suspense>
 
-const NavigationPage = React.lazy(() => import('./components/pages/NavigationPage'))
+const NavigationPage = React.lazy(() => import('./pages/NavigationPage'))
 const asyncNavigationPage = () => <React.Suspense fallback={null}><NavigationPage /></React.Suspense>
 
 
@@ -54,19 +55,21 @@ addDivWithIdToBody()
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={startPageRoute} component={asyncStartPage} />
-        <Route exact path={alertsPageRoute} component={asyncAlertsPage} />
-        <Route exact path={avatarsPageRoute} component={asyncAvatarsPage} />
-        <Route exact path={blankslatePageRoute} component={asyncBlankslatePage} />
-        <Route exact path={breadcrumbPageRoute} component={asyncBreadcrumbPage} />
-        <Route exact path={buttonsPageRoute} component={asyncButtonsPage} />
-        <Route exact path={labelsPageRoute} component={asyncLabelsPage} />
-        <Route exact path={layoutPageRoute} component={asyncLayoutPage} />
-        <Route exact path={navigationPageRoute} component={asyncNavigationPage} />
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <Switch>
+          <Route exact path={startPageRoute} component={asyncStartPage} />
+          <Route exact path={alertsPageRoute} component={asyncAlertsPage} />
+          <Route exact path={avatarsPageRoute} component={asyncAvatarsPage} />
+          <Route exact path={blankslatePageRoute} component={asyncBlankslatePage} />
+          <Route exact path={breadcrumbPageRoute} component={asyncBreadcrumbPage} />
+          <Route exact path={buttonsPageRoute} component={asyncButtonsPage} />
+          <Route exact path={labelsPageRoute} component={asyncLabelsPage} />
+          <Route exact path={layoutPageRoute} component={asyncLayoutPage} />
+          <Route exact path={navigationPageRoute} component={asyncNavigationPage} />
+        </Switch>
+      </ApolloProvider>
+    </BrowserRouter>
   )
 }
 
