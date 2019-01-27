9618cc7bc2a5e15472ad09ccf9b621cf82071d9c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-01-27 15:58:31 +0100

    abstract layout page

---
+++ packages/client/src/pages/LayoutPage/LayoutPage.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import layoutPageDefault from './LayoutPage.render'
+
+jest.mock('react-router-dom', () => {
+  return {
+    Link: () => <div />,
+  }
+})
+
+jest.mock('react-apollo', () => {
+  return {
+    Query: () => <div />,
+  }
+})
+
+describe('<LayoutPage />', () => {
+  describe('Snaphot', () => {
+    test('must match layoutPageDefault', () => {
+      expect(global.renderToJSON(layoutPageDefault)).toMatchSnapshot()
+    })
+  })
+})
