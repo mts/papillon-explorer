1cfa5af1404669891b0d34c8dee2ec474de43024
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-01-27 15:25:38 +0100

    abstract async breadcrumb page

---
+++ packages/client/src/pages/BreadcrumbPage/BreadcrumbPage.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import breadcrumbPageDefault from './BreadcrumbPage.render'
+
+jest.mock('react-router-dom', () => {
+  return {
+    Link: () => <div />,
+  }
+})
+
+jest.mock('react-apollo', () => {
+  return {
+    Query: () => <div />,
+  }
+})
+
+describe('<BreadcrumbPage />', () => {
+  describe('Snaphot', () => {
+    test('must match breadcrumbPageDefault', () => {
+      expect(global.renderToJSON(breadcrumbPageDefault)).toMatchSnapshot()
+    })
+  })
+})
