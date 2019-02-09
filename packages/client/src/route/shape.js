bde94d221d40b97b3c58a6550e30d7270489f9f2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-09 20:33:41 +0100

    extend routeShape

--- packages/client/src/route/shape.js
+++ packages/client/src/route/shape.js
@@ -1,6 +1,14 @@
-import { shape, string, bool } from 'prop-types'
+import { shape, string, bool, node, arrayOf } from 'prop-types'
 
 export const routeShape = shape({
   path: string.isRequired,
   exact: bool.isRequired,
+  component: node.isRequired,
+  routes: arrayOf(
+    shape({
+      path: string.isRequired,
+      exact: bool.isRequired,
+      component: node.isRequired,
+    }),
+  ),
 })
