ed45eb0c7fdebca1a6da413e85cae48cd21c467a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-09 15:02:05 +0100

    abstract intro

--- packages/client/src/pattern/template/StartTemplate/StartTemplate.js
+++ packages/client/src/pattern/template/StartTemplate/StartTemplate.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { version } from '../../../../../../lerna.json'
+import Intro from '../../molecule/banner/Intro'
 import Header from '../../molecule/navigation/Header'
 import SideBar from '../../molecule/navigation/SideBar'
 
@@ -8,21 +9,14 @@ const StartTemplate = () => (
     <Header>Papillon Explorer v{version}</Header>
     <div className="d-flex flex-wrap flex-lg-nowrap">
       <SideBar />
-      <div className="md-order-1 d-flex flex-lg-column flex-auto flex-wrap">
-        <div className="flex-row width-full bg-blue py-3 py-lg-6">
-          <div className="main-content mx-auto py-4 px-3 px-md-6 px-lg-3 text-white">
-            <div className="">
-              <h1 className="f000-light mb-4">Papillon Explorer</h1>
-              <div className="f2-light pb-4">
-                {
-                  "Resources, tooling, and design guidelines for building websites with Papillon, React components for GitHub's Primer Design System"
-                }
-              </div>
-            </div>
-          </div>
+      <Intro>
+        <h1 className="f000-light mb-4">Papillon Explorer</h1>
+        <div className="f2-light pb-4">
+          {
+            "Resources, tooling, and design guidelines for building websites with Papillon, React components for GitHub's Primer Design System"
+          }
         </div>
-        <div className="flex-row main-content my-6 mx-auto px-3 px-md-6 px-lg-3" />
-      </div>
+      </Intro>
     </div>
   </div>
 )
