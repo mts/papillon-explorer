793915487ca6b7d6f1cfc22f4293faf9f8325af5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-09 13:43:28 +0100

    abstract objects links

--- packages/client/src/pattern/molecule/navigation/SideBar/SideBar.js
+++ packages/client/src/pattern/molecule/navigation/SideBar/SideBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { layoutPageRoute } from '../../../../routes'
+import ObjectsLinks from './ObjectsLinks'
 import ComponentsLinks from './ComponentsLinks'
 
 const SideBar = () => (
@@ -323,58 +324,8 @@ const SideBar = () => (
         </ul>
       </li>
       <li className="mt-4 border-bottom" />
-      <li className="px-4 my-4">
-        <a href="/papillon/objects/" className="position-relative f4 d-block text-gray-dark">
-          Objects
-        </a>
-        <ul className="pt-2 list-style-none">
-          <li className="">
-            <a href="/papillon/objects/grid/" className="position-relative f5 py-1 d-block">
-              Grid
-              <span className="position-absolute right-0">
-                <span className="text-green  tooltipped tooltipped-nw" aria-label="New release">
-                  <svg
-                    height="18"
-                    className="octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot v-align-text-top"
-                    viewBox="0 0 8 16"
-                    version="1.1"
-                    width="9"
-                    aria-hidden="true"
-                  >
-                    <path fillRule="evenodd" d="M0 8c0-2.2 1.8-4 4-4s4 1.8 4 4-1.8 4-4 4-4-1.8-4-4z" />
-                  </svg>
-                </span>
-              </span>
-            </a>
-          </li>
-          <li className="">
-            <a href="/papillon/objects/layout/" className="position-relative f5 py-1 d-block">
-              Layout
-              <span className="position-absolute right-0">
-                <span className="text-red  tooltipped tooltipped-nw" aria-label="Deprecated">
-                  <svg
-                    height="18"
-                    className="octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot octicon octicon-primitive-dot v-align-text-top"
-                    viewBox="0 0 8 16"
-                    version="1.1"
-                    width="9"
-                    aria-hidden="true"
-                  >
-                    <path fillRule="evenodd" d="M0 8c0-2.2 1.8-4 4-4s4 1.8 4 4-1.8 4-4 4-4-1.8-4-4z" />
-                  </svg>
-                </span>
-              </span>
-            </a>
-          </li>
-          <li className="">
-            <a href="/papillon/objects/table-object/" className="position-relative f5 py-1 d-block">
-              Table object
-            </a>
-          </li>
-        </ul>
-      </li>
+      <ObjectsLinks />
       <li className="mt-4 border-bottom" />
-
       <ComponentsLinks />
     </ul>
   </nav>
