e0fb831fa315e4b53b58d93529c6f5d5cd38c881
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-10 13:25:32 +0100

    receive location prop and pass it onto NotFoundTemplate

--- packages/client/src/pattern/page/NotFoundPage/NotFoundPage.render.js
+++ packages/client/src/pattern/page/NotFoundPage/NotFoundPage.render.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import NotFoundPage from './NotFoundPage'
 
-const notFoundPageDefault = <NotFoundPage />
+const props = {
+  location: {
+    pathname: 'some-path-name',
+  },
+}
+
+const notFoundPageDefault = <NotFoundPage {...props} />
 
 export default notFoundPageDefault
