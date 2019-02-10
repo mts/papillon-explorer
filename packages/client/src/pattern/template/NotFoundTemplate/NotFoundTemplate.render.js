baa0403c967c78f564f90d65894f29a44211c7c0
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-02-10 12:52:55 +0100

    add NotFoundTemplate

---
+++ packages/client/src/pattern/template/NotFoundTemplate/NotFoundTemplate.render.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NotFoundTemplate from './NotFoundTemplate'
+
+const page = {
+  page: {
+    id: 'not-found',
+    name: 'some-name',
+  },
+}
+
+const notFoundTemplateDefault = <NotFoundTemplate page={page} />
+
+export default notFoundTemplateDefault
