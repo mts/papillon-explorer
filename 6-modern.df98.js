aa76d38aa5c8041b0cd7ff21d91e6ccfa473d711
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-03-01 22:15:19 +0100

    Updates

---
+++ 6-modern.df98.js
@@ -0,0 +1,11 @@
+/*!
+ * @project        Papillon Explorer
+ * @name           6-modern.df98.js
+ * @author         MTS
+ * @build          Fri, Mar 1, 2019 10:15 PM ET
+ * @release        1a1d127789860894f112f7e084ec38921f5ad99c [master]
+ * @copyright      Copyright (c) 2019 MTS
+ *
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{164:function(e,a,t){"use strict";t.r(a);var n=t(0),r=t.n(n),l=t(32),p=t(62),c=t(1),o=t(155),s=t(6);const i=e=>{let a=e.page;return r.a.createElement("div",null,r.a.createElement("h2",null,a.name),r.a.createElement("div",null,"Under construction"," ",r.a.createElement("span",{role:"img","aria-label":"emoji"},"🙂"),r.a.createElement(o.a,{to:s.i}," Back to Home Page")))};i.propTypes={page:c.object.isRequired};var u=i;a.default=(()=>r.a.createElement(l.b,{query:p.a},e=>{let a=e.data;return a.pages&&a.pages.pages?r.a.createElement(u,{page:a.pages.pages.find(e=>"buttons"===e.id)}):null}))}}]);
+//# sourceMappingURL=6-modern.df98.js.map
---
+++ 8-modern.df98.js
@@ -0,0 +1,11 @@
+/*!
+ * @project        Papillon Explorer
+ * @name           8-modern.df98.js
+ * @author         MTS
+ * @build          Fri, Mar 1, 2019 10:15 PM ET
+ * @release        1a1d127789860894f112f7e084ec38921f5ad99c [master]
+ * @copyright      Copyright (c) 2019 MTS
+ *
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{165:function(e,a,t){"use strict";t.r(a);var n=t(0),l=t.n(n),r=t(32),p=t(62),c=t(1),o=t(155),i=t(6);const s=e=>{let a=e.page;return l.a.createElement("div",null,l.a.createElement("h2",null,a.name),l.a.createElement("div",null,"Under construction"," ",l.a.createElement("span",{role:"img","aria-label":"emoji"},"🙂"),l.a.createElement(o.a,{to:i.i}," Back to Home Page")))};s.propTypes={page:c.object.isRequired};var u=s;a.default=(()=>l.a.createElement(r.b,{query:p.a},e=>{let a=e.data;return a.pages&&a.pages.pages?l.a.createElement(u,{page:a.pages.pages.find(e=>"layout"===e.id)}):null}))}}]);
+//# sourceMappingURL=8-modern.df98.js.map
