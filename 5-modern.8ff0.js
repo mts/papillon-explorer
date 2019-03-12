6baad5773e21e435801b4d8a6c19bf7b242d7cf2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-03-12 19:00:38 +0100

    Updates

--- 5-modern.8ff0.js
+++ 5-modern.8ff0.js
@@ -1,11 +1,11 @@
 /*!
  * @project        Papillon Explorer
- * @name           5-modern.df98.js
+ * @name           5-modern.8ff0.js
  * @author         MTS
- * @build          Fri, Mar 1, 2019 10:15 PM ET
- * @release        1a1d127789860894f112f7e084ec38921f5ad99c [master]
+ * @build          Tue, Mar 12, 2019 6:59 PM ET
+ * @release        35ed4824f371918b713a47d9be7e92d5c1bb2b6a [develop]
  * @copyright      Copyright (c) 2019 MTS
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{163:function(e,a,t){"use strict";t.r(a);var n=t(0),r=t.n(n),l=t(32),c=t(62),p=t(1),i=t(155),o=t(6);const s=e=>{let a=e.page;return r.a.createElement("div",null,r.a.createElement("h2",null,a.name),r.a.createElement("div",null,"Under construction"," ",r.a.createElement("span",{role:"img","aria-label":"emoji"},"🙂"),r.a.createElement(i.a,{to:o.i}," Back to Home Page")))};s.propTypes={page:p.object.isRequired};var u=s;a.default=(()=>r.a.createElement(l.b,{query:c.a},e=>{let a=e.data;return a.pages&&a.pages.pages?r.a.createElement(u,{page:a.pages.pages.find(e=>"breadcrumb"===e.id)}):null}))}}]);
-//# sourceMappingURL=5-modern.df98.js.map
+//# sourceMappingURL=5-modern.8ff0.js.map
