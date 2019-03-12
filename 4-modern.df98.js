9c5c9e5e8a8caf07732336ae03a3f287b91b6148
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-03-12 19:05:14 +0100

    Updates

--- 4-modern.df98.js
+++ 4-modern.df98.js
@@ -1,11 +1,11 @@
 /*!
  * @project        Papillon Explorer
- * @name           4-modern.8ff0.js
+ * @name           4-modern.df98.js
  * @author         MTS
- * @build          Tue, Mar 12, 2019 6:59 PM ET
+ * @build          Tue, Mar 12, 2019 7:04 PM ET
  * @release        35ed4824f371918b713a47d9be7e92d5c1bb2b6a [develop]
  * @copyright      Copyright (c) 2019 MTS
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[4],{162:function(e,a,t){"use strict";t.r(a);var n=t(0),l=t.n(n),r=t(32),p=t(62),c=t(1),s=t(155),i=t(6);const o=e=>{let a=e.page;return l.a.createElement("div",null,l.a.createElement("h2",null,a.name),l.a.createElement("div",null,"Under construction"," ",l.a.createElement("span",{role:"img","aria-label":"emoji"},"🙂"),l.a.createElement(s.a,{to:i.i}," Back to Home Page")))};o.propTypes={page:c.object.isRequired};var u=o;a.default=(()=>l.a.createElement(r.b,{query:p.a},e=>{let a=e.data;return a.pages&&a.pages.pages?l.a.createElement(u,{page:a.pages.pages.find(e=>"blankslate"===e.id)}):null}))}}]);
-//# sourceMappingURL=4-modern.8ff0.js.map
+//# sourceMappingURL=4-modern.df98.js.map
--- 9-modern.df98.js
+++ 9-modern.df98.js
@@ -1,11 +1,11 @@
 /*!
  * @project        Papillon Explorer
- * @name           9-modern.8ff0.js
+ * @name           9-modern.df98.js
  * @author         MTS
- * @build          Tue, Mar 12, 2019 6:59 PM ET
+ * @build          Tue, Mar 12, 2019 7:04 PM ET
  * @release        35ed4824f371918b713a47d9be7e92d5c1bb2b6a [develop]
  * @copyright      Copyright (c) 2019 MTS
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[9],{166:function(e,a,t){"use strict";t.r(a);var n=t(0),r=t.n(n),l=t(32),i=t(62),p=t(1),c=t(155),o=t(6);const s=e=>{let a=e.page;return r.a.createElement("div",null,r.a.createElement("h2",null,a.name),r.a.createElement("div",null,"Under construction"," ",r.a.createElement("span",{role:"img","aria-label":"emoji"},"🙂"),r.a.createElement(c.a,{to:o.i}," Back to Home Page")))};s.propTypes={page:p.object.isRequired};var u=s;a.default=(()=>r.a.createElement(l.b,{query:i.a},e=>{let a=e.data;return a.pages&&a.pages.pages?r.a.createElement(u,{page:a.pages.pages.find(e=>"navigation"===e.id)}):null}))}}]);
-//# sourceMappingURL=9-modern.8ff0.js.map
+//# sourceMappingURL=9-modern.df98.js.map
