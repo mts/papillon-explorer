71655fdc3221de14d63770ae5f51cab25ae8bf94
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-08-17 17:37:50 +0200

    update babel config

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-spread',
+  ],
+}
