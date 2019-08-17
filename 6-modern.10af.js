3ca9cea2d8cda7b62f32a2f32d3898a1547936d7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-08-17 18:01:55 +0200

    Updates

--- 6-modern.10af.js
+++ 6-modern.10af.js
@@ -1,11 +1,11 @@
 /*!
  * @project        Papillon Explorer
- * @name           6-modern.a88e.js
+ * @name           6-modern.10af.js
  * @author         MTS
- * @build          Sat, Aug 17, 2019 5:51 PM ET
- * @release        51cbcd1d4e554f806064532b1491de5be142efd1 [master]
+ * @build          Sat, Aug 17, 2019 6:01 PM ET
+ * @release        06fa28d9a84eb2834ed9bff362f7f4c9876eb959 [master]
  * @copyright      Copyright (c) 2019 MTS
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[6],{72:function(e,a,t){"use strict";t.r(a);var n=t(0),r=t.n(n),l=t(63),p=t(26),c=t(4),o=t(19),s=t(7);const i=e=>{let{page:a}=e;return r.a.createElement("div",null,r.a.createElement("h2",null,a.name),r.a.createElement("div",null,"Under construction"," ",r.a.createElement("span",{role:"img","aria-label":"emoji"},"🙂"),r.a.createElement(o.b,{to:s.i}," Back to Home Page")))};i.propTypes={page:c.object.isRequired};var u=i;a.default=()=>r.a.createElement(l.a,{query:p.a},e=>{let{data:a}=e;return a.pages&&a.pages.pages?r.a.createElement(u,{page:a.pages.pages.find(e=>"buttons"===e.id)}):null})}}]);
-//# sourceMappingURL=6-modern.a88e.js.map
+//# sourceMappingURL=6-modern.10af.js.map
--- 8-modern.10af.js
+++ 8-modern.10af.js
@@ -1,11 +1,11 @@
 /*!
  * @project        Papillon Explorer
- * @name           8-modern.a88e.js
+ * @name           8-modern.10af.js
  * @author         MTS
- * @build          Sat, Aug 17, 2019 5:51 PM ET
- * @release        51cbcd1d4e554f806064532b1491de5be142efd1 [master]
+ * @build          Sat, Aug 17, 2019 6:01 PM ET
+ * @release        06fa28d9a84eb2834ed9bff362f7f4c9876eb959 [master]
  * @copyright      Copyright (c) 2019 MTS
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[8],{73:function(e,a,t){"use strict";t.r(a);var n=t(0),l=t.n(n),r=t(63),p=t(26),c=t(4),o=t(19),i=t(7);const s=e=>{let{page:a}=e;return l.a.createElement("div",null,l.a.createElement("h2",null,a.name),l.a.createElement("div",null,"Under construction"," ",l.a.createElement("span",{role:"img","aria-label":"emoji"},"🙂"),l.a.createElement(o.b,{to:i.i}," Back to Home Page")))};s.propTypes={page:c.object.isRequired};var u=s;a.default=()=>l.a.createElement(r.a,{query:p.a},e=>{let{data:a}=e;return a.pages&&a.pages.pages?l.a.createElement(u,{page:a.pages.pages.find(e=>"layout"===e.id)}):null})}}]);
-//# sourceMappingURL=8-modern.a88e.js.map
+//# sourceMappingURL=8-modern.10af.js.map
