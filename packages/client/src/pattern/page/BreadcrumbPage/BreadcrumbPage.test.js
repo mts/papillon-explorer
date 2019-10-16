104238cab4d7e5ec5c49838fb717e3b25f88c91f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 17:13:15 +0200

    extends BreadcrumbPage snapshots

--- packages/client/src/pattern/page/BreadcrumbPage/BreadcrumbPage.test.js
+++ packages/client/src/pattern/page/BreadcrumbPage/BreadcrumbPage.test.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { breadcrumbPageDefault } from './BreadcrumbPage.render'
+import { breadcrumbPageAsync, breadcrumbPageDefault } from './BreadcrumbPage.render'
 
-jest.mock('@apollo/react-hooks', () => {
-  return {
-    useQuery: () => {
-      return {
-        data: {
-          pages: {
-            pages: [{ id: 'breadcrumb', name: 'some-name' }],
-          },
-        },
-      }
-    },
-  }
-})
-
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => {} }))
 jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
+const apolloReactHooksMockObject = require('@apollo/react-hooks')
+
 describe('<BreadcrumbPage />', () => {
+  const pagesDataObject = {
+    data: {
+      pages: {
+        pages: [{ id: 'breadcrumb', name: 'some-name' }],
+      },
+    },
+  }
+
+  afterEach(() => jest.clearAllMocks())
+
   describe('Snaphot', () => {
+    test('must match breadcrumbPageAsync', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue(pagesDataObject)
+      expect(global.renderToJSON(breadcrumbPageAsync)).toMatchSnapshot()
+    })
+
     test('must match breadcrumbPageDefault', () => {
+      expect(global.renderToJSON(breadcrumbPageDefault)).toMatchSnapshot()
+    })
+
+    test('must match breadcrumbPageNull', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue({})
       expect(global.renderToJSON(breadcrumbPageDefault)).toMatchSnapshot()
     })
   })
