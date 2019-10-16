e98aebeef4a2222800eb3b581a99a7331e7fe173
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 16:56:42 +0200

    apply optional chaining to location.pathname

--- packages/client/src/pattern/atom/info/NoMatch/NoMatch.jsx
+++ packages/client/src/pattern/atom/info/NoMatch/NoMatch.jsx
@@ -5,7 +5,7 @@ export const NoMatch = ({ location }) => (
   <>
     <div>
       <h3>
-        No match for <code>{location.pathname}</code>
+        No match for <code>{location?.pathname}</code>
       </h3>
     </div>
   </>
