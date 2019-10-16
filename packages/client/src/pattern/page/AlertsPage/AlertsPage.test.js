827cbf831fe0c8da0934b84e57e133f4cf783c5c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 15:47:38 +0200

    extend alerts test mocks

--- packages/client/src/pattern/page/AlertsPage/AlertsPage.test.js
+++ packages/client/src/pattern/page/AlertsPage/AlertsPage.test.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { alertsPageDefault } from './AlertsPage.render'
 
-jest.mock('react-apollo', () => {
+jest.mock('@apollo/react-hooks', () => {
   return {
-    Query: () => <div />,
+    useQuery: () => {
+      return {
+        data: {
+          pages: {
+            pages: [{ id: 'alerts', name: 'some-name' }],
+          },
+        },
+      }
+    },
   }
 })
+
+jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
 describe('<AlertsPage />', () => {
   describe('Snaphot', () => {
