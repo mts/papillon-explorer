bf4f143b0a685f632a1c21a24435f8e7a38ee150
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 15:47:55 +0200

    extend avatars test mocks

--- packages/client/src/pattern/page/AvatarsPage/AvatarsPage.test.js
+++ packages/client/src/pattern/page/AvatarsPage/AvatarsPage.test.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { avatarsPageDefault } from './AvatarsPage.render'
 
-jest.mock('react-apollo', () => {
+jest.mock('@apollo/react-hooks', () => {
   return {
-    Query: () => <div />,
+    useQuery: () => {
+      return {
+        data: {
+          pages: {
+            pages: [{ id: 'avatars', name: 'some-name' }],
+          },
+        },
+      }
+    },
   }
 })
+
+jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
 describe('<AvatarsPage />', () => {
   describe('Snaphot', () => {
