f338e00050c32dcf73ed911b650727df64962c8a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 17:13:32 +0200

    extends ButtonsPage snapshots

--- packages/client/src/pattern/page/ButtonsPage/ButtonsPage.render.js
+++ packages/client/src/pattern/page/ButtonsPage/ButtonsPage.render.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AsyncButtonsPage } from './AsyncButtonsPage'
 import { ButtonsPage } from './ButtonsPage'
 
+export const buttonsPageAsync = <AsyncButtonsPage />
 export const buttonsPageDefault = <ButtonsPage />
--- packages/client/src/pattern/page/ButtonsPage/ButtonsPage.test.js
+++ packages/client/src/pattern/page/ButtonsPage/ButtonsPage.test.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { buttonsPageDefault } from './ButtonsPage.render'
+import { buttonsPageAsync, buttonsPageDefault } from './ButtonsPage.render'
 
-jest.mock('@apollo/react-hooks', () => {
-  return {
-    useQuery: () => {
-      return {
-        data: {
-          pages: {
-            pages: [{ id: 'buttons', name: 'some-name' }],
-          },
-        },
-      }
-    },
-  }
-})
-
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => {} }))
 jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
+const apolloReactHooksMockObject = require('@apollo/react-hooks')
+
 describe('<ButtonsPage />', () => {
+  const pagesDataObject = {
+    data: {
+      pages: {
+        pages: [{ id: 'buttons', name: 'some-name' }],
+      },
+    },
+  }
+
+  afterEach(() => jest.clearAllMocks())
+
   describe('Snaphot', () => {
+    test('must match buttonsPageAsync', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue(pagesDataObject)
+      expect(global.renderToJSON(buttonsPageAsync)).toMatchSnapshot()
+    })
+
     test('must match buttonsPageDefault', () => {
+      expect(global.renderToJSON(buttonsPageDefault)).toMatchSnapshot()
+    })
+
+    test('must match buttonsPageNull', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue({})
       expect(global.renderToJSON(buttonsPageDefault)).toMatchSnapshot()
     })
   })
