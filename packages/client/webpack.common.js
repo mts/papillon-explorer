8a10f956b5ae978a10f65c1ea0575b514c1e0aa8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 09:49:36 +0200

    bump corejs version to v3.3.2

--- packages/client/webpack.common.js
+++ packages/client/webpack.common.js
@@ -33,7 +33,7 @@ const configureBabelLoader = browserList => {
             {
               modules: false,
               useBuiltIns: 'entry',
-              corejs: '3.0.1',
+              corejs: '3.3.2',
               targets: {
                 browsers: browserList,
               },
