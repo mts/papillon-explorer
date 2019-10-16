d18406423dd07f43ec1864d0a9f20f4efad5d495
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 14:46:27 +0200

    useQuery for start

--- packages/client/src/pattern/page/StartPage/StartPage.jsx
+++ packages/client/src/pattern/page/StartPage/StartPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Query } from 'react-apollo'
+import { useQuery } from '@apollo/react-hooks'
 import { PAGES } from '../../../graphql/query'
 import { StartTemplate } from '../../template/StartTemplate'
 
 export const StartPage = () => {
-  return (
-    <Query query={PAGES}>
-      {({ data }) => (data.pages && data.pages.pages ? <StartTemplate page={data.pages.pages.find(page => page.id === 'start')} /> : null)}
-    </Query>
-  )
+  const { data } = useQuery(PAGES)
+
+  if (data && data.pages && data.pages.pages) {
+    return <StartTemplate page={data.pages.pages.find(page => page.id === 'start')} />
+  }
+
+  return null
 }
 
 export default StartPage
