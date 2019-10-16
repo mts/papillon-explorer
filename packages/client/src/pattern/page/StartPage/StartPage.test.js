6990d8ea7e39dc3c8033f4ca5a1a030d8d1b7a4e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 15:50:29 +0200

    extend start test mocks

--- packages/client/src/pattern/page/StartPage/StartPage.test.js
+++ packages/client/src/pattern/page/StartPage/StartPage.test.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { startPageDefault } from './StartPage.render'
 
-jest.mock('react-apollo', () => {
+jest.mock('@apollo/react-hooks', () => {
   return {
-    Query: () => <div />,
+    useQuery: () => {
+      return {
+        data: {
+          pages: {
+            pages: [{ id: 'start', name: 'some-name' }],
+          },
+        },
+      }
+    },
   }
 })
+
+jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
 describe('<StartPage />', () => {
   describe('Snaphot', () => {
