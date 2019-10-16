adfdb8068110eb853f85182433c24f080ebb2bb9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 15:20:29 +0200

    extend babel plugins

--- packages/client/webpack.common.js
+++ packages/client/webpack.common.js
@@ -43,8 +43,11 @@ const configureBabelLoader = browserList => {
         ],
         plugins: [
           '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-object-rest-spread',
+          '@babel/plugin-proposal-optional-catch-binding',
+          '@babel/plugin-proposal-optional-chaining',
           '@babel/plugin-syntax-dynamic-import',
-          '@babel/plugin-transform-runtime',
+          '@babel/plugin-transform-classes',
           '@babel/plugin-transform-spread',
         ],
       },
