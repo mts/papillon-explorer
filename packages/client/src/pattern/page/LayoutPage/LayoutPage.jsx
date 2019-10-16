114f8e8877ef506449b5c70de0f9842e472e56fd
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 14:42:26 +0200

    useQuery for layout

--- packages/client/src/pattern/page/LayoutPage/LayoutPage.jsx
+++ packages/client/src/pattern/page/LayoutPage/LayoutPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Query } from 'react-apollo'
+import { useQuery } from '@apollo/react-hooks'
 import { PAGES } from '../../../graphql/query'
 import { LayoutTemplate } from '../../template/LayoutTemplate'
 
 export const LayoutPage = () => {
-  return (
-    <Query query={PAGES}>
-      {({ data }) =>
-        data.pages && data.pages.pages ? <LayoutTemplate page={data.pages.pages.find(page => page.id === 'layout')} /> : null
-      }
-    </Query>
-  )
+  const { data } = useQuery(PAGES)
+
+  if (data && data.pages && data.pages.pages) {
+    return <LayoutTemplate page={data.pages.pages.find(page => page.id === 'layout')} />
+  }
+
+  return null
 }
 
 export default LayoutPage
