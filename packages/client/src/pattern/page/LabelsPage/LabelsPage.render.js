d2e059837ffb9c93df5c03b4aa5ee65bc23c7dbc
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 17:13:50 +0200

    extends LabelsPage snapshots

--- packages/client/src/pattern/page/LabelsPage/LabelsPage.render.js
+++ packages/client/src/pattern/page/LabelsPage/LabelsPage.render.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AsyncLabelsPage } from './AsyncLabelsPage'
 import { LabelsPage } from './LabelsPage'
 
+export const labelsPageAsync = <AsyncLabelsPage />
 export const labelsPageDefault = <LabelsPage />
--- packages/client/src/pattern/page/LabelsPage/LabelsPage.test.js
+++ packages/client/src/pattern/page/LabelsPage/LabelsPage.test.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { labelsPageDefault } from './LabelsPage.render'
+import { labelsPageAsync, labelsPageDefault } from './LabelsPage.render'
 
-jest.mock('@apollo/react-hooks', () => {
-  return {
-    useQuery: () => {
-      return {
-        data: {
-          pages: {
-            pages: [{ id: 'labels', name: 'some-name' }],
-          },
-        },
-      }
-    },
-  }
-})
-
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => {} }))
 jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
+const apolloReactHooksMockObject = require('@apollo/react-hooks')
+
 describe('<LabelsPage />', () => {
+  const pagesDataObject = {
+    data: {
+      pages: {
+        pages: [{ id: 'labels', name: 'some-name' }],
+      },
+    },
+  }
+
+  afterEach(() => jest.clearAllMocks())
+
   describe('Snaphot', () => {
+    test('must match labelsPageAsync', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue(pagesDataObject)
+      expect(global.renderToJSON(labelsPageAsync)).toMatchSnapshot()
+    })
+
     test('must match labelsPageDefault', () => {
+      expect(global.renderToJSON(labelsPageDefault)).toMatchSnapshot()
+    })
+
+    test('must match labelsPageNull', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue({})
       expect(global.renderToJSON(labelsPageDefault)).toMatchSnapshot()
     })
   })
