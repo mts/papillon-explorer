afdec912908b203508bf81ad3f088ed70850ffca
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 17:12:19 +0200

    extends AlertsPage snapshots

--- packages/client/src/pattern/page/AlertsPage/AlertsPage.render.js
+++ packages/client/src/pattern/page/AlertsPage/AlertsPage.render.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AsyncAlertsPage } from './AsyncAlertsPage'
 import { AlertsPage } from './AlertsPage'
 
+export const alertsPageAsync = <AsyncAlertsPage />
 export const alertsPageDefault = <AlertsPage />
--- packages/client/src/pattern/page/AlertsPage/AlertsPage.test.js
+++ packages/client/src/pattern/page/AlertsPage/AlertsPage.test.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { alertsPageDefault } from './AlertsPage.render'
+import { alertsPageAsync, alertsPageDefault } from './AlertsPage.render'
 
-jest.mock('@apollo/react-hooks', () => {
-  return {
-    useQuery: () => {
-      return {
-        data: {
-          pages: {
-            pages: [{ id: 'alerts', name: 'some-name' }],
-          },
-        },
-      }
-    },
-  }
-})
-
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => {} }))
 jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
+const apolloReactHooksMockObject = require('@apollo/react-hooks')
+
 describe('<AlertsPage />', () => {
+  const pagesDataObject = {
+    data: {
+      pages: {
+        pages: [{ id: 'alerts', name: 'some-name' }],
+      },
+    },
+  }
+
+  afterEach(() => jest.clearAllMocks())
+
   describe('Snaphot', () => {
+    test('must match alertsPageAsync', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue(pagesDataObject)
+      expect(global.renderToJSON(alertsPageAsync)).toMatchSnapshot()
+    })
+
     test('must match alertsPageDefault', () => {
+      expect(global.renderToJSON(alertsPageDefault)).toMatchSnapshot()
+    })
+
+    test('must match alertsPageNull', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue({})
       expect(global.renderToJSON(alertsPageDefault)).toMatchSnapshot()
     })
   })
