11b8335ef31da06142ea1f751f80b608285e392c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 18:35:38 +0200

    partition StartPage page

--- packages/client/src/pattern/page/StartPage/__tests__/StartPage.int.test.js
+++ packages/client/src/pattern/page/StartPage/__tests__/StartPage.int.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { startPageAsync, startPageDefault } from './StartPage.render'
+import { startPageAsync, startPageDefault } from './StartPage.int.render'
 
 jest.mock('@apollo/react-hooks', () => ({ useQuery: () => {} }))
 jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
