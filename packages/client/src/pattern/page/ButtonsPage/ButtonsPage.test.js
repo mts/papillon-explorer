e2a4c5bc0bd5edaacf1c6e1857a1960bb086dc98
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 15:48:57 +0200

    extend buttons test mocks

--- packages/client/src/pattern/page/ButtonsPage/ButtonsPage.test.js
+++ packages/client/src/pattern/page/ButtonsPage/ButtonsPage.test.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { buttonsPageDefault } from './ButtonsPage.render'
 
-jest.mock('react-apollo', () => {
+jest.mock('@apollo/react-hooks', () => {
   return {
-    Query: () => <div />,
+    useQuery: () => {
+      return {
+        data: {
+          pages: {
+            pages: [{ id: 'buttons', name: 'some-name' }],
+          },
+        },
+      }
+    },
   }
 })
+
+jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
 describe('<ButtonsPage />', () => {
   describe('Snaphot', () => {
