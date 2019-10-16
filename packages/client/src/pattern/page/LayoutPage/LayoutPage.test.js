17906aba7671bb939574d2fed9f9222949bff474
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 17:14:09 +0200

    extends LayoutPage snapshots

--- packages/client/src/pattern/page/LayoutPage/LayoutPage.test.js
+++ packages/client/src/pattern/page/LayoutPage/LayoutPage.test.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { layoutPageDefault } from './LayoutPage.render'
+import { layoutPageAsync, layoutPageDefault } from './LayoutPage.render'
 
-jest.mock('@apollo/react-hooks', () => {
-  return {
-    useQuery: () => {
-      return {
-        data: {
-          pages: {
-            pages: [{ id: 'layout', name: 'some-name' }],
-          },
-        },
-      }
-    },
-  }
-})
-
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => {} }))
 jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
+const apolloReactHooksMockObject = require('@apollo/react-hooks')
+
 describe('<LayoutPage />', () => {
+  const pagesDataObject = {
+    data: {
+      pages: {
+        pages: [{ id: 'layout', name: 'some-name' }],
+      },
+    },
+  }
+
+  afterEach(() => jest.clearAllMocks())
+
   describe('Snaphot', () => {
+    test('must match layoutPageAsync', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue(pagesDataObject)
+      expect(global.renderToJSON(layoutPageAsync)).toMatchSnapshot()
+    })
+
     test('must match layoutPageDefault', () => {
+      expect(global.renderToJSON(layoutPageDefault)).toMatchSnapshot()
+    })
+
+    test('must match layoutPageNull', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue({})
       expect(global.renderToJSON(layoutPageDefault)).toMatchSnapshot()
     })
   })
