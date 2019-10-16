0375334d6a736197e1a861734ee6102907a76f65
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 17:12:38 +0200

    extends AvatarsPage snapshots

--- packages/client/src/pattern/page/AvatarsPage/AvatarsPage.render.js
+++ packages/client/src/pattern/page/AvatarsPage/AvatarsPage.render.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AsyncAvatarsPage } from './AsyncAvatarsPage'
 import { AvatarsPage } from './AvatarsPage'
 
+export const avatarsPageAsync = <AsyncAvatarsPage />
 export const avatarsPageDefault = <AvatarsPage />
--- packages/client/src/pattern/page/AvatarsPage/AvatarsPage.test.js
+++ packages/client/src/pattern/page/AvatarsPage/AvatarsPage.test.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { avatarsPageDefault } from './AvatarsPage.render'
+import { avatarsPageAsync, avatarsPageDefault } from './AvatarsPage.render'
 
-jest.mock('@apollo/react-hooks', () => {
-  return {
-    useQuery: () => {
-      return {
-        data: {
-          pages: {
-            pages: [{ id: 'avatars', name: 'some-name' }],
-          },
-        },
-      }
-    },
-  }
-})
-
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => {} }))
 jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
+const apolloReactHooksMockObject = require('@apollo/react-hooks')
+
 describe('<AvatarsPage />', () => {
+  const pagesDataObject = {
+    data: {
+      pages: {
+        pages: [{ id: 'avatars', name: 'some-name' }],
+      },
+    },
+  }
+
+  afterEach(() => jest.clearAllMocks())
+
   describe('Snaphot', () => {
+    test('must match avatarsPageAsync', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue(pagesDataObject)
+      expect(global.renderToJSON(avatarsPageAsync)).toMatchSnapshot()
+    })
+
     test('must match avatarsPageDefault', () => {
+      expect(global.renderToJSON(avatarsPageDefault)).toMatchSnapshot()
+    })
+
+    test('must match avatarsPageNull', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue({})
       expect(global.renderToJSON(avatarsPageDefault)).toMatchSnapshot()
     })
   })
