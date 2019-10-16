894f0a6a3231665eeadecca133817f17e00d4da9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 15:48:17 +0200

    extend blankslate test mocks

--- packages/client/src/pattern/page/BlankslatePage/BlankslatePage.test.js
+++ packages/client/src/pattern/page/BlankslatePage/BlankslatePage.test.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { blankslatePageDefault } from './BlankslatePage.render'
 
-jest.mock('react-apollo', () => {
+jest.mock('@apollo/react-hooks', () => {
   return {
-    Query: () => <div />,
+    useQuery: () => {
+      return {
+        data: {
+          pages: {
+            pages: [{ id: 'blankslate', name: 'some-name' }],
+          },
+        },
+      }
+    },
   }
 })
+
+jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
 describe('<BlankslatePage />', () => {
   describe('Snaphot', () => {
