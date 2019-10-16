3976ed88dbcafa0407354ede36606004b666ef28
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 17:12:58 +0200

    extends BlankslatePage snapshots

--- packages/client/src/pattern/page/BlankslatePage/BlankslatePage.render.js
+++ packages/client/src/pattern/page/BlankslatePage/BlankslatePage.render.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AsyncBlankslatePage } from './AsyncBlankslatePage'
 import { BlankslatePage } from './BlankslatePage'
 
+export const blankslatePageAsync = <AsyncBlankslatePage />
 export const blankslatePageDefault = <BlankslatePage />
--- packages/client/src/pattern/page/BlankslatePage/BlankslatePage.test.js
+++ packages/client/src/pattern/page/BlankslatePage/BlankslatePage.test.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { blankslatePageDefault } from './BlankslatePage.render'
+import { blankslatePageAsync, blankslatePageDefault } from './BlankslatePage.render'
 
-jest.mock('@apollo/react-hooks', () => {
-  return {
-    useQuery: () => {
-      return {
-        data: {
-          pages: {
-            pages: [{ id: 'blankslate', name: 'some-name' }],
-          },
-        },
-      }
-    },
-  }
-})
-
+jest.mock('@apollo/react-hooks', () => ({ useQuery: () => {} }))
 jest.mock('react-router-dom', () => ({ Link: () => <div>some link</div> }))
 
+const apolloReactHooksMockObject = require('@apollo/react-hooks')
+
 describe('<BlankslatePage />', () => {
+  const pagesDataObject = {
+    data: {
+      pages: {
+        pages: [{ id: 'blankslate', name: 'some-name' }],
+      },
+    },
+  }
+
+  afterEach(() => jest.clearAllMocks())
+
   describe('Snaphot', () => {
+    test('must match blankslatePageAsync', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue(pagesDataObject)
+      expect(global.renderToJSON(blankslatePageAsync)).toMatchSnapshot()
+    })
+
     test('must match blankslatePageDefault', () => {
+      expect(global.renderToJSON(blankslatePageDefault)).toMatchSnapshot()
+    })
+
+    test('must match blankslatePageNull', () => {
+      jest.spyOn(apolloReactHooksMockObject, 'useQuery').mockReturnValue({})
       expect(global.renderToJSON(blankslatePageDefault)).toMatchSnapshot()
     })
   })
