7e9b401bea3f051513ca07f8de762d0362437724
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 17:00:36 +0200

    extend StartPage snapshots

--- packages/client/src/pattern/page/StartPage/StartPage.render.js
+++ packages/client/src/pattern/page/StartPage/StartPage.render.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AsyncStartPage } from './AsyncStartPage'
 import { StartPage } from './StartPage'
 
+export const startPageAsync = <AsyncStartPage />
 export const startPageDefault = <StartPage />
